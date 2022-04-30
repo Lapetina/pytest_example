7ad27a988bfd75cf33740be07ae3d3d7af30cf77
lapetina <victor.lrs95@gmail.com>
2022-04-30 13:52:35 -0300

    feat: xfail class 16

---
+++ Pytest_topics/test_xfail.py
@@ -0,0 +1,26 @@
+import sys
+
+import pytest
+
+
+def test_strjoin():
+    s1 = 'Python, Pytest and Automation'
+    l1 = ['Python,Pytest', 'and', 'Automation']
+    l2 = ['Python', 'Pytest and Automation']
+
+    assert ' '.join(l1) == s1
+
+
+@pytest.mark.xfail(raises=IndexError, reason='know issue')
+def test_str04():
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+
+    assert letters[100]
+
+
+@pytest.mark.xfail(sys.platform == 'linux', reason='works only in linux')
+def test_str05():
+    letters = 'abcd'
+    num = 1234
+
+    assert letters + num == 'abcd1234'
